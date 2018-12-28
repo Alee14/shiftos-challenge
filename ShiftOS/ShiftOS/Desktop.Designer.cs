8debadb818e43c9492f5cabce003d59375caaad0
Alkaline Thunder <theplexgate@gmail.com>
2018-12-28 00:11:22 -0500

    Add the desktop panel to the desktop.

--- ShiftOS/ShiftOS/Desktop.Designer.cs
+++ ShiftOS/ShiftOS/Desktop.Designer.cs
@@ -28,13 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.DesktopPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // DesktopPanel
+            // 
+            this.DesktopPanel.BackColor = System.Drawing.Color.Gray;
+            this.DesktopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.DesktopPanel.ForeColor = System.Drawing.Color.Black;
+            this.DesktopPanel.Location = new System.Drawing.Point(0, 0);
+            this.DesktopPanel.Name = "DesktopPanel";
+            this.DesktopPanel.Size = new System.Drawing.Size(800, 24);
+            this.DesktopPanel.TabIndex = 0;
+            // 
+            // Desktop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DesktopPanel);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Desktop";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(0)))), ((int)(((byte)(1)))));
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel DesktopPanel;
     }
 }
 
