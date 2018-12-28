3cde475dc1cf291f3643f61f30e2d8158969344c
Alkaline Thunder <theplexgate@gmail.com>
2018-12-28 00:06:43 -0500

    Initial commit: System context work.

---
+++ ShiftOS/ShiftOS/SystemContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace ShiftOS
+{
+    public class SystemContext : IDisposable
+    {
+        private Desktop _desktop = null;
+
+        public void Dispose()
+        {
+            _desktop = null;
+        }
+
+        public void Initialize()
+        {
+            // We can't initialize the game twice.
+            if (_desktop != null)
+                throw new InvalidOperationException("ShiftOS is already initialized.");
+
+            // Set up WinForms to run normally.
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using (_desktop = new Desktop())
+            {
+                // Run Windows Forms.
+                Application.Run(_desktop);
+            }
+        }
+    }
+}
